532ca4db07a7522bca7cfd32f2fdbce3c17a2e20
IanCedrick123 <168614547+IanCedrick123@users.noreply.github.com>
2024-05-03 15:33:42 +0800

    Add files via upload

---
+++ ecom.js
@@ -0,0 +1,13 @@
+function addToCart(productName) {
+            alert(productName + " added to cart!");
+        }
+function buyNow(productName) {
+    var confirmBuy = confirm("Do you want to buy " + productName + " now?");
+    if (confirmBuy) {
+		var nameUser = prompt("Please enter the receiver's name:");
+        var paymentMethod = prompt("Please enter your payment method (e.g., credit card, PayPal):");
+        var shippingAddress = prompt("Please enter your shipping address:");
+         alert("Thank you for your purchase!");
+	  window.location.href = "main.html";
+    }
+}
